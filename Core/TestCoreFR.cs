4e9b195664dbdc98362b34d563ec18e9f6ccd038
Пользователь <@DESKTOP-2L0KGCL>
2023-03-01 16:06:30 +0300

    Добавьте файлы проекта.

---
+++ Core/TestCoreFR.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace TestProject.Core
+{
+    public static class TestCoreFR
+    {
+        internal static object MyCore;
+
+        public static Frame? GoFrameTest { get; set; }
+    }
+}
